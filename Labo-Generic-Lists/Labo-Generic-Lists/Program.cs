98cf017bb17b4ffa4ef2cb73f82135fe7a58b1c5
LorenzoLatinnePXL <11701160@student.pxl.be>
2024-12-10 10:13:16 +0100

    Exercise finished.

---
+++ Labo-Generic-Lists/Labo-Generic-Lists/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo_Generic_Lists
+{
+    internal class Program
+    {
+        private static List<string> users = new List<string>();
+        private static string user;
+        private static int optie;
+        private static bool isValidOption;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Voeg een naam toe.");
+            Console.WriteLine("2. Toon alle namen.");
+            Console.WriteLine("3. Zoek een naam.");
+            Console.WriteLine("4. Verwijder een naam.");
+            Console.WriteLine("5. Toon het aantal namen.");
+            Console.WriteLine("6. Stoppen.");
+
+            do
+            {
+                do
+                {
+                Console.WriteLine("Kies een optie: ");
+                isValidOption = int.TryParse(Console.ReadLine(), out optie);
+                } while (!isValidOption);
+
+                switch (optie)
+                {
+                    // Oefening 1
+                    case 1:
+                        AddUsersToList();
+                        break;
+                    // Oefening 2
+                    case 2:
+                        ShowAllNamesInList();
+                        break;
+                    // Oefening 3
+                    case 3:
+                        CheckIfNameIsInList();
+                        break;
+                    // Oefening 4
+                    case 4:
+                        DeleteNameFromList();
+                        break;
+                    // Oefening 5
+                    case 5:
+                        ShowAmountOfNamesInList();
+                        break;
+                    default:
+                        Console.WriteLine("Choose a valid option from the menu.");
+                        break;
+                }
+            } while (optie != 6);
+        }
+        
+        // Oefening 1
+        private static void AddUsersToList()
+        {
+            do
+            {
+                Console.Write("Geef een naam: ");
+                user = Console.ReadLine();
+            } while (String.IsNullOrEmpty(user));
+
+            users.Add(user);
+        }
+
+        // Oefening 2
+        private static void ShowAllNamesInList()
+        {
+            foreach (string name in users)
+            {
+                Console.Write($"{name} ");
+            }
+            Console.WriteLine();
+        }
+
+        // Oefening 3
+        private static void CheckIfNameIsInList()
+        {
+        Console.Write("Geef een naam: ");
+            string search = Console.ReadLine();
+
+            if (users.IndexOf(search) != -1)
+            {
+                Console.WriteLine($"Gevonden op index: {users.IndexOf(search)}");
+            } 
+            else
+            {
+                Console.WriteLine("Naam niet gevonden in de lijst.");
+            }
+        }
+
+        // Oefening 4
+        private static void DeleteNameFromList()
+        {
+            string deleteName;
+            do
+            {
+                Console.Write("Geef een naam om te verwijderen: ");
+                deleteName = Console.ReadLine();
+            } while (String.IsNullOrEmpty(deleteName));
+
+            users.Remove(deleteName);
+        }
+
+        // Oefening 5
+        private static void ShowAmountOfNamesInList()
+        {
+            Console.WriteLine($"Aantal namen in de lijst: {users.Count()}");
+        }
+    }
+}
---
+++ Labo-Generic-Lists/Labo-Generic-Lists/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Labo-Generic-Lists")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PXL")]
+[assembly: AssemblyProduct("Labo-Generic-Lists")]
+[assembly: AssemblyCopyright("Copyright © PXL 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("742eb1c5-b1ab-43e1-a4f5-d532b4373873")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
